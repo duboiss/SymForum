092378541d538e029488493feba8b41224162afa
Coton <coton@github.com>
2019-09-25 19:17:41 +0200

    Add MessageType form and integrate it in thread template

--- src/Controller/ThreadController.php
+++ src/Controller/ThreadController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
 use App\Entity\Thread;
+use App\Form\MessageType;
 use App\Repository\MessageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,15 +17,40 @@ class ThreadController extends AbstractController
      * @Route("/forums/threads/{id}-{slug}", name="forums.thread")
      * @param Thread $thread
      * @param MessageRepository $repo
+     * @param Request $request
      * @return Response
+     * @throws \Exception
      */
-    public function index(Thread $thread, MessageRepository $repo)
+    public function index(Thread $thread, MessageRepository $repo, Request $request)
     {
         $messages = $repo->findMessagesByThreadWithAuthor($thread);
 
+
+        $message = new Message();
+        $form = $this->createForm(MessageType::class, $message);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $user = $this->getUser();
+
+            $message->setAuthor($user);
+            $message->setPublishedAt(new \DateTime());
+            $message->setThread($thread);
+
+            $em->persist($message);
+            $em->flush();
+
+            return $this->redirectToRoute('forums.thread', [
+                'id' => $thread->getId(),
+                'slug' => $thread->getSlug()
+            ]);
+        }
+
         return $this->render('forums/thread.html.twig', [
             'thread' => $thread,
-            'messages' => $messages
+            'messages' => $messages,
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/MessageType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Message;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MessageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('content', CKEditorType::class, [
+                'label' => false
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Message::class,
+        ]);
+    }
+}
