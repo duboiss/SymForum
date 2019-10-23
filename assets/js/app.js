d9930aac80a34611ff60c4701769cc147e8388cd
Coton <coton@github.com>
2019-10-23 20:27:48 +0200

    Add css files in webpack encore

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,5 @@
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
-
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
