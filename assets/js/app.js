bc2750ff9a15d672c89581e312b978cfd6230a3e
Coton <coton@github.com>
2019-10-26 14:41:51 +0200

    Remove fontawesome js in app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,6 @@
 import '../css/app.css';
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import '@fortawesome/fontawesome-free/js/all.js';
 
 import $ from 'jquery';
 import 'bootstrap';
