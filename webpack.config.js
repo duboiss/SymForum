0bd27a8f702c31e9b5d5014cc96d00833adb363c
Coton <coton@github.com>
2019-10-27 19:51:16 +0100

    Add modnav in thread template

--- webpack.config.js
+++ webpack.config.js
@@ -24,8 +24,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
-    //.addEntry('page1', './assets/js/page1.js')
-    //.addEntry('page2', './assets/js/page2.js')
+    .addStyleEntry('modnav', './assets/css/modnav.css')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
