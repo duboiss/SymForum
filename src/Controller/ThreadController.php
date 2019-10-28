9622f3b6a85ed90eec6641c1bd5d9e627ffeca50
Coton <coton@github.com>
2019-10-28 20:16:19 +0100

    Moderators (and higher) can lock or unlock a thread in modnav

--- src/Controller/ThreadController.php
+++ src/Controller/ThreadController.php
@@ -7,6 +7,8 @@ use App\Entity\Thread;
 use App\Form\MessageType;
 use App\Repository\MessageRepository;
 use App\Service\AntispamService;
+use Doctrine\Common\Persistence\ObjectManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -72,6 +74,54 @@ class ThreadController extends BaseController
             'thread' => $thread,
             'messages' => $messages,
             'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/forums/threads/{id}/lock", name="thread.lock")
+     * @IsGranted("ROLE_MODERATOR")
+     * @param Thread $thread
+     * @param ObjectManager $manager
+     * @return Response
+     */
+    public function lock(Thread $thread, ObjectManager $manager): Response
+    {
+        if ($thread->getLocked()) {
+            $this->addCustomFlash('error', 'Sujet', 'Ce sujet est déjà fermé !');
+        } else {
+            $thread->setLocked(true);
+            $manager->flush();
+
+            $this->addCustomFlash('success', 'Sujet', 'Le sujet a été fermé !');
+        }
+
+        return $this->redirectToRoute('thread.show', [
+            'id' => $thread->getId(),
+            'slug' => $thread->getSlug()
+        ]);
+    }
+
+    /**
+     * @Route("/forums/threads/{id}/unlock", name="thread.unlock")
+     * @IsGranted("ROLE_MODERATOR")
+     * @param Thread $thread
+     * @param ObjectManager $manager
+     * @return Response
+     */
+    public function unlock(Thread $thread, ObjectManager $manager): Response
+    {
+        if (!$thread->getLocked()) {
+            $this->addCustomFlash('error', 'Sujet', 'Ce sujet est déjà ouvert !');
+        } else {
+            $thread->setLocked(false);
+            $manager->flush();
+
+            $this->addCustomFlash('success', 'Sujet', 'Le sujet a été ouvert !');
+        }
+
+        return $this->redirectToRoute('thread.show', [
+            'id' => $thread->getId(),
+            'slug' => $thread->getSlug()
         ]);
     }
 }
