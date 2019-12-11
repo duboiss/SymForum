c28263acc9c4249cdcec8931773e053d432ef1ec
Coton <coton@github.com>
2019-12-11 19:25:33 +0100

    Replace stof/doctrine-extensions-bundle by antishov/doctrine-extensions-bundle package

--- config/bundles.php
+++ config/bundles.php
@@ -18,6 +18,6 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Exercise\HTMLPurifierBundle\ExerciseHTMLPurifierBundle::class => ['all' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
-    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
