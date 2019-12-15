57f758ddb6b6a2fb272054e17717b0579c232dcf
Coton <coton@github.com>
2019-12-15 14:40:26 +0100

    Remove unused use statement (Contraints) in Thread entity

--- src/Entity/Thread.php
+++ src/Entity/Thread.php
@@ -8,7 +8,6 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ThreadRepository")
