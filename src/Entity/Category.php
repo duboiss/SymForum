befca15f87d6e84f57e611a9482c435210af4257
Coton <coton@github.com>
2019-12-15 14:45:03 +0100

    Fix categories edition in panel : title cannot be null (prevent whitespaces)

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -62,7 +62,7 @@ class Category
         return $this->title;
     }
 
-    public function setTitle(string $title): self
+    public function setTitle(?string $title): self
     {
         $this->title = $title;
 
