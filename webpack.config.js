5d8df720c75791b239ab59a0ef10c2b7ddcb1c2e
Coton <coton@github.com>
2020-02-01 14:31:28 +0100

    Disable build notifications in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ Encore
      * https://symfony.com/doc/current/frontend.html#adding-more-features
      */
     .cleanupOutputBeforeBuild()
-    .enableBuildNotifications()
+    //.enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
     // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
