57dee7b6d3086cfa2ff010a8f3790a65fcda5b53
Steven <29716703+DuboisS@users.noreply.github.com>
2020-02-05 19:23:22 +0100

    CKEditorType : move required and attr form variables in buildView method

--- src/Form/Type/CKEditorType.php
+++ src/Form/Type/CKEditorType.php
@@ -4,6 +4,8 @@ namespace App\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CKEditorType extends AbstractType
@@ -11,13 +13,19 @@ class CKEditorType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'purify_html' => true,
-            'label' => false,
-            'attr' => [
-                'class' => 'editor',
-            ],
-            'required' => false
+            'label' => false
         ]);
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $attr = $view->vars['attr'];
+        $class = isset($attr['class']) ? $attr['class'].' ' : '';
+        $class .= 'editor';
+
+        $attr['class'] = $class;
+        $view->vars['attr'] = $attr;
+        $view->vars['required'] = false;
     }
 
     public function getParent()
