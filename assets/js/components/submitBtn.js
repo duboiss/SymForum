9e8343ec1716b93e2f68178386386d04fd8be2e0
Steven <29716703+DuboisS@users.noreply.github.com>
2020-04-24 12:37:43 +0200

    Add a .js-submit-link class (same thing that .js-submit-btn) to prevent multiple link clicks

--- assets/js/components/submitBtn.js
+++ assets/js/components/submitBtn.js
@@ -1,11 +1,16 @@
 import $ from 'jquery';
 
+const spinner = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Chargement...';
+
 $(document).ready(function() {
     $('form').submit(function() {
-        let $btn = $(this).find('.js-submit-button');
+        let $btn = $(this).find('.js-submit-btn');
         $btn.prop('disabled', true);
-        $btn.html(
-            `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Chargement...`
-        );
+        $btn.html(spinner);
+    });
+
+    $('a.js-submit-link').click(function() {
+        $(this).addClass('disabled');
+        $(this).html(spinner);
     });
 });
