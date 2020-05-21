c59c58d42b7b1d812900fd200699c407f0aa7a28
Steven <29716703+DuboisS@users.noreply.github.com>
2020-05-21 17:29:09 +0200

    Add UserControllerTest

---
+++ tests/Controller/UserControllerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Liip\TestFixturesBundle\Test\FixturesTrait;
+
+class UserControllerTest extends AbstractControllerTest
+{
+    use NeedLoginTrait, FixturesTrait;
+
+    private function logAsDemo(): void
+    {
+        $users = $this->loadFixtureFiles([dirname(__DIR__) . '/Fixtures/users.yaml']);
+        $this->logIn($this->client, $users['user_demo']);
+    }
+
+    public function testDisplayProfile(): void
+    {
+        $this->logAsDemo();
+        $this->responseIsSuccessful('/user/admin');
+        $this->responseIsSuccessful('/user/demo');
+
+        self::assertSelectorTextContains('h3', 'demo');
+        self::assertSelectorTextContains('p', 'Date d\'inscription');
+        self::assertSelectorTextContains('p', 'Dernière activité');
+        self::assertSelectorTextContains('p', 'Nombre de messages');
+        self::assertSelectorTextContains('h4', 'Derniers sujets créés');
+    }
+
+    public function testDisplayUserThreads(): void
+    {
+        $this->logAsDemo();
+        $this->responseIsSuccessful('/user/demo/threads');
+        self::assertSelectorTextContains('h3', 'demo');
+    }
+
+    public function testDisplayUserMessages(): void
+    {
+        $this->logAsDemo();
+        $this->responseIsSuccessful('/user/demo/messages');
+        self::assertSelectorTextContains('h3', 'demo');
+    }
+}
