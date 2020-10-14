4b42e6bbcfda5eec7b31c8103bbee33c5aae55b1
Steven DUBOIS <git@duboiss.fr>
2020-10-14 19:47:10 +0200

    Add more rules to php-cs-fixer

--- tests/ApplicationAvailabilityFunctionalTest.php
+++ tests/ApplicationAvailabilityFunctionalTest.php
@@ -4,7 +4,6 @@ namespace App\Tests;
 
 use App\Entity\User;
 use App\Tests\Controller\AbstractControllerTest;
-use Generator;
 use Liip\TestFixturesBundle\Test\FixturesTrait;
 use Symfony\Component\HttpFoundation\Response;
 
