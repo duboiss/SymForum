08cf7866bebc42f3b7272d9839ca4b38a079bb25
Steven DUBOIS <git@duboiss.fr>
2020-10-31 17:33:20 +0100

    Add unsigned option to PrimaryKeyTrait

--- src/Entity/Traits/PrimaryKeyTrait.php
+++ src/Entity/Traits/PrimaryKeyTrait.php
@@ -7,9 +7,9 @@ use Doctrine\ORM\Mapping as ORM;
 trait PrimaryKeyTrait
 {
     /**
-     * @ORM\Id())
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer", options={"unsigned"=true})
      */
     private ?int $id = null;
 
